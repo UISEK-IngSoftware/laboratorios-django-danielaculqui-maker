4e0cdbcbed2015cbf160af89ba200517521adb2e
danielaculqui-maker <daniela.culqui@uisek.edu.ec>
2026-05-11 20:49:19 -0500

    Laboratorio 5 - Deber Completado

--- lab8/settings.py
+++ lab8/settings.py
@@ -122,3 +122,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / 'media'
--- pokedex/admin.py
+++ pokedex/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
+from .models import Pokemon, Trainer
 
-from .models import Pokemon
-admin.site.register(Pokemon)
 # Register your models here.
+@admin.register(Pokemon)
+class PokemonAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Trainer)
+class TrainerAdmin(admin.ModelAdmin):
+    pass
+
